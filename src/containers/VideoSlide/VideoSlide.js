ff7337db8fc3709a70f73bc9344e0382d13ba73e
Kid Zeppelin <ccr2k@Kids-MacBook-Pro.local>
2021-03-12 08:06:49 -0800

    added all files to staging area , no cganges

---
+++ src/containers/VideoSlide/VideoSlide.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Aux';
+
+class VideoSlide extends Component {
+    render () {
+        return (
+            <div></div>
+        );
+
+    }
+}
